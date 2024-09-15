642cc5945a86499f853a83639c46c017af6722e9
da <sayess.da@gmail.com>
2024-09-15 18:06:19 +0100

    update

--- Main.tf
+++ Main.tf
@@ -6,4 +6,14 @@ provider "aws" {
 resource "aws_s3_bucket" "bucketle4k" {
   bucket = "bucketle4k"  # S3 bucket name
 }
-#
+# Create SNS Topic
+resource "aws_sns_topic" "personal_data_alert" {
+  name = "PersonalDataAlert"
+}
+
+# Subscribe email to the SNS Topic
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.personal_data_alert.arn
+  protocol  = "email"
+  endpoint  = "sayess.da@gmail.com"  # Email to subscribe
+}
